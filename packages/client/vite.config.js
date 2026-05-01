894482da29694fbf2c223bef11f3ccb23be86526
Abhishek Sharma <2k22.cscys.2211373@gmail.com>
2026-05-01 10:03:17 +0530

    Added all the fixes.

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://taskflowserver-production-3812.up.railway.app',
         changeOrigin: true,
       },
     },
